767130a29050a36569b764ff33de028895e2c44e
Kayzhe Tsar <kayzhe@backbase.com>
2021-03-16 11:06:59 +0100

    Move validators to a class

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,33 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { containsLowerUppercaseValidator } from 'src/app/custom-validators/contains-lower-uppercase.validator';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { CustomValidators } from 'src/app/custom-validators/custom-validators';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.sass']
+  styleUrls: ['./sign-up.component.sass'],
 })
 export class SignUpComponent implements OnInit {
+  signUpForm = new FormGroup(
+    {
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        this.customValidators.containsLowerUppercaseValidator(),
+      ]),
+    },
+    {
+      validators: [this.customValidators.containsNameInPasswordValidator()],
+    }
+  );
 
-  signUpForm = new FormGroup({
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.minLength(8), containsLowerUppercaseValidator])
-  });
-
-  get firstName() { return this.signUpForm.get('firstName'); }
-  get lastName() { return this.signUpForm.get('lastName'); }
-  get email() { return this.signUpForm.get('email'); }
-  get password() { return this.signUpForm.get('password'); }
-
-  constructor() { }
- 
-  ngOnInit(): void {
+  get firstName(): AbstractControl | null {
+    return this.signUpForm.get('firstName');
   }
+  get lastName(): AbstractControl | null {
+    return this.signUpForm.get('lastName');
+  }
+  get email(): AbstractControl | null {
+    return this.signUpForm.get('email');
+  }
+  get password(): AbstractControl | null {
+    return this.signUpForm.get('password');
+  }
+
+  constructor(private customValidators : CustomValidators) {}
+
+  ngOnInit(): void {}
 
   onSubmit() {
     console.log(this.password);
+    console.log(this.signUpForm);
   }
-
 }
---
+++ src/app/custom-validators/custom-validators.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class CustomValidators {
+  public containsLowerUppercaseValidator(): ValidatorFn {
+    return (formControl: AbstractControl) => {
+      let regex = /(?=.*[a-z])(?=.*[A-Z])/g;
+      if (regex.test(formControl.value)) {
+        return null;
+      } else {
+        return { doesNotContainLowercaseAndUppercase: true };
+      }
+    };
+  }
+
+  public containsNameInPasswordValidator(): ValidatorFn {
+    return (formGroup: AbstractControl) => {
+      const firstName = formGroup.get('firstName')!.value;
+      const lastName = formGroup.get('lastName')!.value;
+      const password = formGroup.get('password')!.value;
+
+      if (!password) return null;
+
+      if (firstName && password.toLowerCase().includes(firstName.toLowerCase())) {
+        formGroup.get('password')?.setErrors({ passwordContainsName: true });
+        return null;
+      } else if (lastName && password.toLowerCase().includes(lastName.toLowerCase())) {
+        formGroup.get('password')?.setErrors({ passwordContainsName: true });
+        return null;
+      } else {
+        return null;
+      }
+    };
+  }
+}
